94b520b054ead26c8e55f7ba4a07ab329c60d48e
YanMatiasBraga <yanmatiasbraga97@gmail.com>
2018-10-04 09:46:47 -0300

    4/10 android

--- app/src/main/java/com/techsix/techsix_android/CadastrarHorarioActivity.java
+++ app/src/main/java/com/techsix/techsix_android/CadastrarHorarioActivity.java
@@ -3,12 +3,15 @@ package com.techsix.techsix_android;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 /**
  * Created by android on 01/10/2018.
  */
 
 public class CadastrarHorarioActivity extends AppCompatActivity {
+
+    private EditText etDataCadastro, etHoraCadastro;
 
 
 
@@ -17,7 +20,6 @@ public class CadastrarHorarioActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastrar_horario);
-
 
 
     }
